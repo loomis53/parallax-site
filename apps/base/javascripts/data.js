d34eec5ddacd2158690edcf788a4683ef9c4a9af
Greg Statton <gstatton@bechtel.com>
2012-11-09 15:35:30 -0800

    various updates to cataogue and main page

---
+++ apps/base/javascripts/data.js
@@ -0,0 +1,5 @@
+var completeResults = [{ "permalink": "gbetr/", "image": "gbetr/logo.png", "title": "gBETR" }, 
+{ "permalink": "iforms/", "image": "iforms/logo.png", "title": "iForms" }, 
+{ "permalink": "towers/", "image": "../logos/tower.png", "title": "Towers" }, 
+{ "permalink": "commodities/", "image": "../logos/commodities.png", "title": "Commodities" }, 
+{ "permalink": "icontracts/", "image": "../logos/icontracts.png", "title": "iContract" }];
